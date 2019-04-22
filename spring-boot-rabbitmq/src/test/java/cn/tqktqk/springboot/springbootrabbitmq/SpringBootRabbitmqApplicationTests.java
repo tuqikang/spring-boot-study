10181bb64c08bde74f7d6ea66594f5864ae71b08
tqk <1282995880@qq.com>
2019-04-22 14:43:19 +0800

    rabbit测试

--- spring-boot-rabbitmq/src/test/java/cn/tqktqk/springboot/springbootrabbitmq/SpringBootRabbitmqApplicationTests.java
+++ spring-boot-rabbitmq/src/test/java/cn/tqktqk/springboot/springbootrabbitmq/SpringBootRabbitmqApplicationTests.java
@@ -1,16 +1,25 @@
 package cn.tqktqk.springboot.springbootrabbitmq;
 
+import cn.tqktqk.springboot.springbootrabbitmq.service.HelloSenderService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.concurrent.TimeUnit;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBootRabbitmqApplicationTests {
 
+    @Autowired
+    private HelloSenderService senderService;
+
     @Test
-    public void contextLoads() {
+    public void contextLoads() throws InterruptedException {
+        senderService.send1();
+        senderService.send2();
     }
 
 }
