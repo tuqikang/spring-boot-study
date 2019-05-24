c0ee147fd3ea965394dc6cf412684a3cda7c2a33
tqk <1282995880@qq.com>
2019-05-24 09:25:07 +0800

    将amqp模版换成rabbit模版

--- spring-boot-rabbitmq/src/main/java/cn/tqktqk/springboot/springbootrabbitmq/service/HelloSenderService.java
+++ spring-boot-rabbitmq/src/main/java/cn/tqktqk/springboot/springbootrabbitmq/service/HelloSenderService.java
@@ -4,11 +4,9 @@ import cn.tqktqk.springboot.springbootrabbitmq.config.MqFeildConst;
 import cn.tqktqk.springboot.springbootrabbitmq.model.UserInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
 
 /**
  * ___________ ________     ____  __.
@@ -26,25 +24,25 @@ public class HelloSenderService {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
-    private AmqpTemplate amqpTemplate;
+    private RabbitTemplate rabbitTemplate;
 
     public void send(UserInfo userInfo){
 //        String message = "hello "+ LocalDateTime.now();
 //        logger.info("send info :"+message);
-//        amqpTemplate.convertAndSend(MqFeildConst.QUEUE_NAME,message);
+//        rabbitTemplate.convertAndSend(MqFeildConst.QUEUE_NAME,message);
         logger.info(userInfo.toString());
-        amqpTemplate.convertAndSend(MqFeildConst.QUEUE_NAME,userInfo);
+        rabbitTemplate.convertAndSend(MqFeildConst.QUEUE_NAME,userInfo);
     }
 
     public void send1() {
         String context = "hi, i am message 1";
         System.out.println("Sender : " + context);
-        this.amqpTemplate.convertAndSend("exchange", "topic.message.one", context);
+        this.rabbitTemplate.convertAndSend("exchange", "topic.message.one", context);
     }
 
     public void send2() {
         String context = "hi, i am messages 2";
         System.out.println("Sender : " + context);
-        this.amqpTemplate.convertAndSend("exchange", "topic.message.two", context);
+        this.rabbitTemplate.convertAndSend("exchange", "topic.message.two", context);
     }
 }
